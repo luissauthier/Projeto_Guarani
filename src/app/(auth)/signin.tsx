b10abe7462b99797a3eb0874de5b3119c14de2f8
Luis Henrique S. Sauthier <luis.sauthier@universo.univates.br>
2025-08-18 20:45:59 -0300

    teste

--- src/app/(auth)/signin.tsx
+++ src/app/(auth)/signin.tsx
@@ -156,7 +156,7 @@ const styles = StyleSheet.create({
         elevation: 4,
     },
     buttonText: {
-        color: '#FFF',
+        color: '#000',
         fontSize: 18, // Tamanho da fonte maior
         fontWeight: 'bold',
         textTransform: 'uppercase', // Texto em maiúsculas
